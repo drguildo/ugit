5db234d4f14af81b37023d5a25158aebafd104ae
Simon Morgan <sjm@sjm.io>
2020-09-20 19:17:13 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use clap::App;
+
+fn main() {
+    let matches = App::new(clap::crate_name!())
+        .about(clap::crate_description!())
+        .author(clap::crate_authors!())
+        .version(clap::crate_version!())
+        .subcommand(clap::SubCommand::with_name("init"))
+        .get_matches();
+
+    if let Some(_matches) = matches.subcommand_matches("init") {
+        init();
+    }
+}
+
+fn init() {
+    println!("Hello, World!")
+}
