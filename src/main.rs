6abcfbb55f429ee7ca4d35d2954184c26c17a520
Simon Morgan <sjm@sjm.io>
2020-09-20 22:41:48 +0100

    Implement hash-object

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use clap::App;
+use clap::{App, Arg, SubCommand};
 
 mod ugit;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let matches = App::new(clap::crate_name!())
         .about(clap::crate_description!())
         .author(clap::crate_authors!())
         .version(clap::crate_version!())
-        .subcommand(clap::SubCommand::with_name("init"))
+        .subcommand(SubCommand::with_name("init"))
+        .subcommand(
+            SubCommand::with_name("hash-object").arg(Arg::with_name("filename").required(true)),
+        )
         .get_matches();
 
     if let Some(_matches) = matches.subcommand_matches("init") {
-        ugit::data::init()?;
+        ugit::data::init();
+        std::process::exit(0);
     }
 
-    Ok(())
+    if let Some(matches) = matches.subcommand_matches("hash-object") {
+        let filename = matches.value_of("filename").unwrap();
+        let contents = std::fs::read(filename).expect("Failed to read file contents");
+        let object_hash: String = ugit::data::hash_object(&contents);
+        println!("{}", object_hash);
+        std::process::exit(0);
+    }
 }
--- src/ugit/data.rs
+++ src/ugit/data.rs
@@ -1,6 +1,30 @@
+use std::path::PathBuf;
+use std::{fmt::Write, fs};
+
+use sha1::{digest, Digest, Sha1};
+
 const GIT_DIR: &str = ".ugit";
 
-pub fn init() -> std::io::Result<()> {
-    std::fs::create_dir(GIT_DIR)?;
-    Ok(())
+pub fn init() {
+    let mut path = PathBuf::from(GIT_DIR);
+    fs::create_dir(&path).expect("Failed to create .ugit directory");
+
+    path.push("objects");
+    fs::create_dir(&path).expect("Unable to create objects directory");
+}
+
+pub fn hash_object(data: &Vec<u8>) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(data);
+    let result: digest::generic_array::GenericArray<u8, _> = hasher.finalize();
+
+    let mut oid = String::new();
+    for byte in result {
+        write!(&mut oid, "{:x}", byte).expect("Unable to construct object filename");
+    }
+
+    let path: PathBuf = [GIT_DIR, "objects", &oid].iter().collect();
+    fs::write(path, data).expect("Failed to write file data");
+
+    oid
 }
