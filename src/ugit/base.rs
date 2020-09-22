b972d785ef8c1b423245d32533df31a469b67e76
Simon Morgan <sjm@sjm.io>
2020-09-22 22:40:42 +0100

    Add an is_ignored function
    Returns true if the path should be ignored/skipped.

--- src/ugit/base.rs
+++ src/ugit/base.rs
@@ -1,6 +1,10 @@
-use std::{fs, path::Path};
+use std::{fs, path::Component, path::Path};
 
 pub fn write_tree(path: &Path) {
+    if is_ignored(path) {
+        return;
+    }
+
     for entry in fs::read_dir(path).expect("Failed to read directory") {
         let entry = entry.expect("Failed to read directory entry");
         let path = entry.path();
@@ -10,4 +14,9 @@ pub fn write_tree(path: &Path) {
             println!("{:?}", path);
         }
     }
+}
+
+fn is_ignored(path: &Path) -> bool {
+    path.components()
+        .any(|c| c == Component::Normal(".ugit".as_ref()))
 }
