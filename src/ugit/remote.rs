6bc8dd61917b7febeab86baf3e5f044d89ce174b
Simon Morgan <sjm@sjm.io>
2020-12-20 19:42:11 +0000

    Retrieve remote refs in a separate function

--- src/ugit/remote.rs
+++ src/ugit/remote.rs
@@ -1,14 +1,22 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
 use super::{data, DEFAULT_REPO};
 
 pub fn fetch(remote_path: &Path) {
     println!("Will fetch the following refs:");
 
+    for refname in get_remote_refs(remote_path, Some("refs/heads")).keys() {
+        println!("- {}", refname);
+    }
+}
+
+fn get_remote_refs(remote_path: &Path, prefix: Option<&str>) -> HashMap<String, Option<String>> {
     let mut remote_object_store = remote_path.to_path_buf();
     remote_object_store.push(DEFAULT_REPO);
 
-    for (refname, _) in data::get_refs(&remote_object_store, Some("refs/heads"), true) {
-        println!("- {}", refname);
+    let mut result = HashMap::new();
+    for (refname, reference) in data::get_refs(&remote_object_store, prefix, true) {
+        result.insert(refname, reference.value);
     }
+    result
 }
